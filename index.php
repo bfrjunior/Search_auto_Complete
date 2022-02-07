30c88dee109f10fc4b2074766d84ba6248b418a8
Fernando Rocha <beneditofernandor@gmail.com>
2022-02-07 10:44:03 -0300

    ini

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>    
+        <input type="text">
+    </head>
+    <body>
+        <input type="text"  name="busca" id="busca"/>
+        <div id="resultado">
+            
+        </div>
+        <script type="text/javascript" src="jquery.min.js"></script>
+        <script type="text/javascript" src="script.js"></script>
+    </body>
+</html>
